b22cf2ccf30b7c8bea25efbb5545f25667d0c8fe
P4quiton <ggcswordpixe@gmail.com>
2025-02-19 23:02:29 -0600

    Pa conectar BaseDeDatos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,6 @@ module com.example.tap2025 {
     exports com.example.tap2025;
     exports com.example.tap2025.vistas;
     opens com.example.tap2025.vistas to javafx.fxml;
+    requires mysql.connector.j;
+    requires java.sql;
 }
